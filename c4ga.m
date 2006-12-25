468b772ee9b5abb6204c466f8ac8e26be82cb518
Stig Brautaset <stig@brautaset.org>
2006-12-25 00:47:05 +0000

    * s/CocoaGames/Auberon/g * Fix path to SBPerception * Add targets for ga & states

--- c4ga.m
+++ c4ga.m
@@ -1,29 +1,29 @@
 /*
 Copyright (C) 2006 Stig Brautaset. All rights reserved.
 
-This file is part of CocoaGames.
+This file is part of Auberon.
 
-CocoaGames is free software; you can redistribute it and/or modify
+Auberon is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-CocoaGames is distributed in the hope that it will be useful,
+Auberon is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with CocoaGames; if not, write to the Free Software
+along with Auberon; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
 #import <Cocoa/Cocoa.h>
+#import <SBAlphaBeta/SBAlphaBeta.h>
 
 #import "Connect4NNEvaluator.h"
 #import "Connect4State.h"
-#import "AlphaBeta.h"
 
 NSArray *breed(NSArray *parents)
 {
@@ -96,7 +96,7 @@ void rate(NSArray *arr)
             int p = random() % 2;
             int ch = p == 1 ? 'A' : 'a';
             id st = [[Connect4State new] autorelease];
-            id g = [[AlphaBeta alloc] initWithState:st];
+            id g = [[SBAlphaBeta alloc] initWithState:st];
             
             do {
                 [st setEvaluator:(player[p])];
