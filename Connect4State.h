a441db1210d0cfc632ce1bbd6f7ecb19a90da039
Stig Brautaset <stig@brautaset.org>
2007-07-08 00:21:00 +0000

    push to trunk

--- Connect4State.h
+++ Connect4State.h
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
-#import <SBAlphaBeta/SBAlphaBeta.h>
+#import <AlphaBeta/AlphaBeta.h>
 
 #define COLS 7
 #define ROWS 6
